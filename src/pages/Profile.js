40416bd4ce4f3744b93fed988eac8b91a6a77c4e
DesmondTo <e0550619@u.nus.edu>
2021-12-08 15:38:40 +0800

    Add password changing logic

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,8 @@
-import { useRef } from "react";
+import { useRef, useContext } from "react";
 
+import { useNavigate } from "react-router-dom";
+
+import AuthContext from "../store/auth-context";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -8,25 +11,56 @@ import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 
 export default function Profile() {
+  const navigate = useNavigate();
   const newPasswordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    // Future improvement: add validation here
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBuRBQUOL9pRCG_uAPC6c-CBibziO4f7-w",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          password: enteredNewPassword,
+          returnSecureToken: false,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((res) => {
+      // Assumption: Always succeed
+      // Future improvement: Add handling of error here
+      navigate("/home", { replace: true });
+    });
+  };
 
   return (
     <Card>
       <CardContent>
         <Typography variant="h5">Enter Your New Password</Typography>
-        <form>
+        <form onSubmit={submitHandler}>
           <TextField
             error={false}
             fullWidth
             label="New Password"
             required
+            type="password"
             variant="outlined"
             inputRef={newPasswordInputRef}
           />
+          <CardActions>
+            <Button type="submit" variant="outlined">
+              Change Password
+            </Button>
+          </CardActions>
         </form>
-        <CardActions>
-          <Button>Change Password</Button>
-        </CardActions>
       </CardContent>
     </Card>
   );
