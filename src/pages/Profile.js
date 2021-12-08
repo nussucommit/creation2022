bd1db3b74989b165bed4571ac443920f46dd0c61
DesmondTo <e0550619@u.nus.edu>
2021-12-08 15:28:33 +0800

    Add change password form

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,33 @@
+import { useRef } from "react";
+
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+
 export default function Profile() {
-    return (
-        <div>
-            Profile page. Use to change password.
-        </div>
-    );
+  const newPasswordInputRef = useRef();
+
+  return (
+    <Card>
+      <CardContent>
+        <Typography variant="h5">Enter Your New Password</Typography>
+        <form>
+          <TextField
+            error={false}
+            fullWidth
+            label="New Password"
+            required
+            variant="outlined"
+            inputRef={newPasswordInputRef}
+          />
+        </form>
+        <CardActions>
+          <Button>Change Password</Button>
+        </CardActions>
+      </CardContent>
+    </Card>
+  );
 }
