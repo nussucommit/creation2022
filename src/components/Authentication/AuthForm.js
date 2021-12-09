751723e2c97e051ac84050111c8441eb813fbeb7
DesmondTo <e0550619@u.nus.edu>
2021-12-09 21:40:15 +0800

    Add input validations

--- src/components/Authentication/AuthForm.js
+++ src/components/Authentication/AuthForm.js
@@ -10,26 +10,78 @@ import Typography from "@mui/material/Typography";
 
 import AuthContext from "../../store/auth-context";
 import authErrorResponses from "../../constants/Authentication/AuthErrorResponses";
+import validateInput, {
+  compareString,
+} from "../../validations/InputValidation";
+import inputPatterns from "../../constants/Authentication/InputPatterns";
 
 export default function AuthForm({ isSignin }) {
   const navigate = useNavigate();
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+  const confirmPasswordInputRef = useRef();
 
   const authCtx = useContext(AuthContext);
 
   const [isLogin] = useState(isSignin);
   const [isLoading, setIsLoading] = useState(false);
 
+  const [enteredEmailIsValid, setEnteredEmailIsValid] = useState(false);
+  const [enteredPasswordIsValid, setEnteredPasswordIsValid] = useState(false);
+  const [enteredConfirmPasswordIsValid, setEnteredConfirmPasswordIsValid] =
+    useState(false);
+  const [submitButtonClicked, setSubmitButtonClicked] = useState(false);
+
   const submitHandler = (event) => {
     event.preventDefault();
 
+    /** Input validation */
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
-    // optional: add validation here
+    const enteredConfirmPassword = isLogin
+      ? ""
+      : confirmPasswordInputRef.current.value;
 
+    setSubmitButtonClicked(true);
+
+    const emailIsValid = validateInput(
+      enteredEmail,
+      inputPatterns["email"],
+      "The email provided must be NUS email and cannot be blank!"
+    );
+
+    const passwordIsValid =
+      isLogin ||
+      validateInput(
+        enteredPassword,
+        inputPatterns["password"],
+        "The password must have minimum eight characters, at least one letter and one number!"
+      );
+
+    const confirmPasswordIsMatched =
+      isLogin ||
+      compareString(
+        enteredPassword,
+        enteredConfirmPassword,
+        "Please make sure your password and confirm password match!"
+      );
+
+    emailIsValid ? setEnteredEmailIsValid(true) : setEnteredEmailIsValid(false);
+
+    passwordIsValid
+      ? setEnteredPasswordIsValid(true)
+      : setEnteredPasswordIsValid(false);
+
+    confirmPasswordIsMatched
+      ? setEnteredConfirmPasswordIsValid(true)
+      : setEnteredConfirmPasswordIsValid(false);
+
+    if (!emailIsValid || !passwordIsValid || !confirmPasswordIsMatched) {
+      return;
+    }
     setIsLoading(true);
 
+    /** Backend validation */
     let url;
     if (isLogin) {
       url =
@@ -70,7 +122,7 @@ export default function AuthForm({ isSignin }) {
           new Date().getTime() + +data.expiresIn * 1000
         );
         authCtx.login(data.idToken, expirationTime.toISOString());
-        navigate('/home', { replace: true });
+        navigate("/home", { replace: true });
       })
       .catch((err) => {
         alert(err.message);
@@ -81,11 +133,11 @@ export default function AuthForm({ isSignin }) {
     <Card raised>
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          {isSignin ? "Sign in to continue" : "Sign up to continue"}
+          {isLogin ? "Sign in to continue" : "Sign up to continue"}
         </Typography>
         <form onSubmit={submitHandler}>
           <TextField
-            error={false}
+            error={submitButtonClicked && !enteredEmailIsValid}
             fullWidth
             helperText="Example: e1234567@u.nus.edu"
             label="NUS Email"
@@ -94,7 +146,7 @@ export default function AuthForm({ isSignin }) {
             inputRef={emailInputRef}
           />
           <TextField
-            error={false}
+            error={submitButtonClicked && !enteredPasswordIsValid}
             fullWidth
             label="Password"
             required
@@ -102,25 +154,26 @@ export default function AuthForm({ isSignin }) {
             variant="outlined"
             inputRef={passwordInputRef}
           />
-          {/* {!isSignin && (
+          {!isLogin && (
             <TextField
-              error={false}
+              error={submitButtonClicked && !enteredConfirmPasswordIsValid}
               fullWidth
               label="Confirm Password"
               required
               type="password"
               variant="outlined"
+              inputRef={confirmPasswordInputRef}
             />
-          )} */}
+          )}
           <CardActions>
-            <Button component={NavLink} to={isSignin ? "/signup" : "/signin"}>
-              {isSignin ? "Create new account" : "Login with existing account"}
+            <Button component={NavLink} to={isLogin ? "/signup" : "/signin"}>
+              {isLogin ? "Create new account" : "Login with existing account"}
             </Button>
           </CardActions>
           <CardActions>
             {!isLoading && (
               <Button type="submit">
-                {isSignin ? "Login" : "Create Account"}
+                {isLogin ? "Login" : "Create Account"}
               </Button>
             )}
             {isLoading && <p>Sending request...</p>}
