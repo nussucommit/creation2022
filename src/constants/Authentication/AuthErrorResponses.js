aad1332bbcb325f9c8efdb30f0669b4e30f5fbc6
DesmondTo <e0550619@u.nus.edu>
2021-12-09 21:40:15 +0800

    Add input validations

--- src/constants/Authentication/AuthErrorResponses.js
+++ src/constants/Authentication/AuthErrorResponses.js
@@ -1,6 +1,7 @@
 const authErrorResponses = {
     "INVALID_EMAIL" : "The email provided is not registered yet",
     "INVALID_PASSWORD" : "Wrong password, please try again",
+    "EMPTY_EMAIL" : "Do not leave your email blank please :'(",
 }
 
 export default authErrorResponses;
