1880c158539bffbe0f26be7fb407214f85bf55e0
DesmondTo <e0550619@u.nus.edu>
2021-12-09 22:45:33 +0800

    Add more error response

--- src/constants/Authentication/AuthErrorResponses.js
+++ src/constants/Authentication/AuthErrorResponses.js
@@ -1,6 +1,7 @@
 const authErrorResponses = {
     "EMAIL_NOT_FOUND": "The email provided is not registered yet",
     "INVALID_PASSWORD" : "Wrong password, please try again",
+    "CREDENTIAL_TOO_OLD_LOGIN_AGAIN" : "Please logout and re-login again to continue",
 }
 
 export default authErrorResponses;
