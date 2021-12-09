39a384d1f4c72fd56b7229438d16840f075a69b0
DesmondTo <e0550619@u.nus.edu>
2021-12-09 11:45:18 +0800

    Improve authentication error message

---
+++ src/constants/Authentication/AuthErrorResponses.js
@@ -0,0 +1,6 @@
+const authErrorResponses = {
+    "INVALID_EMAIL" : "The email provided is not registered yet",
+    "INVALID_PASSWORD" : "Wrong password, please try again",
+}
+
+export default authErrorResponses;
