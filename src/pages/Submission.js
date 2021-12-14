94daf9dd72a87e80dbbed0f54842b930eb28a99a
DesmondTo <e0550619@u.nus.edu>
2021-12-14 19:55:03 +0800

    Extract out submission component

--- src/pages/Submission.js
+++ src/pages/Submission.js
@@ -1,53 +1,12 @@
-import { useState } from "react";
+import FileUploadForm from "../components/Submission/FileUploadForm";
 
-import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
-
-import { storage } from "../firebase/firebase";
-
-export default function Submission() {
-  const [progress, setProgress] = useState(0);
-  const [imageURL, setImageURL] = useState("");
-
-  const submitFileHandler = (event) => {
-    event.preventDefault();
-    const file = event.target.files[0];
-    uploadFiles(file);
-  };
-
-  const uploadFiles = (file) => {
-    if (!file) {
-      return;
-    }
-    const storageRef = ref(storage, `images/${file.name}`);
-    const uploadTask = uploadBytesResumable(storageRef, file);
-
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        const prog = Math.round(
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-        );
-        setProgress(prog);
-      },
-      (error) => alert(error.message),
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          setImageURL(downloadURL);
-        });
-      }
-    );
-  };
-
+function Submission() {
   return (
-    <div className="App">
-      <form>
-        <input type="file" onChange={submitFileHandler} />
-      </form>
-      <hr />
-      <h2>Uploading done {progress}%</h2>
-      {imageURL !== "" && (
-        <img src={imageURL} alt="Submitted competition file" />
-      )}
-    </div>
+    <>
+      <FileUploadForm fileType="image" />
+      <FileUploadForm fileType="psd" />
+    </>
   );
 }
+
+export default Submission;
