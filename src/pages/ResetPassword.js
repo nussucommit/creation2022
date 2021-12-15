cc824bfd84ebfacae4c7bb6f96cb1119a76fe382
DesmondTo <e0550619@u.nus.edu>
2021-12-15 23:48:43 +0800

    Implement forgot password feature

--- src/pages/ResetPassword.js
+++ src/pages/ResetPassword.js
@@ -1,5 +1,60 @@
+import { useState, useRef, useContext } from "react";
+
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import SendIcon from "@mui/icons-material/Send";
+import TextField from "@mui/material/TextField";
+
+import AuthContext from "../store/auth-context";
+import { validateInput } from "../validations/validate-input";
+
 function ResetPassword() {
-  return <div>Reset password page</div>;
+  const authCtx = useContext(AuthContext);
+  const emailInputRef = useRef();
+  const [enteredEmailIsValid, setEnteredEmailIsValid] = useState(false);
+  const [submitButtonClicked, setSubmitButtonClicked] = useState(false);
+
+  const sendPasswordResetEmailHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+
+    setSubmitButtonClicked(true);
+
+    const { emailIsValid } = validateInput(enteredEmail, "", "", true);
+    setEnteredEmailIsValid(emailIsValid);
+
+    if (!emailIsValid) {
+      return;
+    }
+
+    authCtx.resetPasswordByEmail(enteredEmail);
+  };
+
+  return (
+    <Card raised>
+      <CardContent>
+        <form onSubmit={sendPasswordResetEmailHandler}>
+          <TextField
+            error={submitButtonClicked && !enteredEmailIsValid}
+            variant="standard"
+            label="Enter your email here"
+            placeholder="e1234567@u.nus.edu"
+            required
+            fullWidth
+            inputRef={emailInputRef}
+          ></TextField>
+          <CardActions>
+            <Button type="submit" variant="contained" endIcon={<SendIcon />}>
+              Send reset password email
+            </Button>
+          </CardActions>
+        </form>
+      </CardContent>
+    </Card>
+  );
 }
 
 export default ResetPassword;
