ad2de7929221c69826963e9ace51c217db55d47f
DesmondTo <e0550619@u.nus.edu>
2021-12-15 18:28:52 +0800

    Polish code

--- src/components/Authentication/AuthForm.js
+++ src/components/Authentication/AuthForm.js
@@ -1,6 +1,6 @@
 import { useState, useRef, useContext } from "react";
 
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -9,131 +9,61 @@ import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 
 import AuthContext from "../../store/auth-context";
-import authErrorResponses from "../../constants/Authentication/AuthErrorResponses";
-import validateInput, {
-  compareString,
-} from "../../validations/InputValidation";
-import inputPatterns from "../../constants/Authentication/InputPatterns";
+import { validateInput } from "../../validations/validate-input";
 
 export default function AuthForm({ isSignin }) {
-  const navigate = useNavigate();
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
   const confirmPasswordInputRef = useRef();
 
   const authCtx = useContext(AuthContext);
-
-  const [isLogin] = useState(isSignin);
   const [isLoading, setIsLoading] = useState(false);
-
   const [enteredEmailIsValid, setEnteredEmailIsValid] = useState(false);
   const [enteredPasswordIsValid, setEnteredPasswordIsValid] = useState(false);
-  const [enteredConfirmPasswordIsValid, setEnteredConfirmPasswordIsValid] =
+  const [enteredConfirmPasswordIsMatch, setEnteredConfirmPasswordIsMatch] =
     useState(false);
   const [submitButtonClicked, setSubmitButtonClicked] = useState(false);
 
   const submitHandler = (event) => {
     event.preventDefault();
 
-    /** Input validation */
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
-    const enteredConfirmPassword = isLogin
+    const enteredConfirmPassword = isSignin
       ? ""
       : confirmPasswordInputRef.current.value;
 
     setSubmitButtonClicked(true);
 
-    const emailIsValid = validateInput(
-      enteredEmail,
-      inputPatterns["email"],
-      "The email provided must be NUS email and cannot be blank!"
-    );
-
-    const passwordIsValid =
-      isLogin ||
+    const { emailIsValid, passwordIsValid, confirmPasswordIsMatch } =
       validateInput(
-        enteredPassword,
-        inputPatterns["password"],
-        "The password must have minimum eight characters, at least one letter and one number!"
-      );
-
-    const confirmPasswordIsMatched =
-      isLogin ||
-      compareString(
+        enteredEmail,
         enteredPassword,
         enteredConfirmPassword,
-        "Please make sure your password and confirm password match!"
+        isSignin
       );
 
-    emailIsValid ? setEnteredEmailIsValid(true) : setEnteredEmailIsValid(false);
+    setEnteredEmailIsValid(emailIsValid);
+    setEnteredPasswordIsValid(passwordIsValid);
+    setEnteredConfirmPasswordIsMatch(confirmPasswordIsMatch);
 
-    passwordIsValid
-      ? setEnteredPasswordIsValid(true)
-      : setEnteredPasswordIsValid(false);
-
-    confirmPasswordIsMatched
-      ? setEnteredConfirmPasswordIsValid(true)
-      : setEnteredConfirmPasswordIsValid(false);
-
-    if (!emailIsValid || !passwordIsValid || !confirmPasswordIsMatched) {
+    if (!emailIsValid || !passwordIsValid || !confirmPasswordIsMatch) {
       return;
     }
     setIsLoading(true);
 
-    /** Backend validation */
-    let url;
-    if (isLogin) {
-      url =
-        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBuRBQUOL9pRCG_uAPC6c-CBibziO4f7-w";
-    } else {
-      url =
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBuRBQUOL9pRCG_uAPC6c-CBibziO4f7-w";
-    }
+    isSignin
+      ? authCtx.signin(enteredEmail, enteredPassword)
+      : authCtx.signup(enteredEmail, enteredPassword);
 
-    fetch(url, {
-      method: "POST",
-      body: JSON.stringify({
-        email: enteredEmail,
-        password: enteredPassword,
-        returnSecureToken: true,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => {
-        setIsLoading(false);
-        if (res.ok) {
-          return res.json();
-        } else {
-          return res.json().then((data) => {
-            let errorMessage = "Authentication failed!";
-            if (data && data.error && data.error.message) {
-              errorMessage = data.error.message;
-            }
-            // Future improvement: using a snackbar instead
-            throw new Error(authErrorResponses[errorMessage]);
-          });
-        }
-      })
-      .then((data) => {
-        const expirationTime = new Date(
-          new Date().getTime() + +data.expiresIn * 1000
-        );
-        authCtx.login(data.idToken, enteredEmail, enteredPassword, expirationTime.toISOString());
-        navigate("/home", { replace: true });
-      })
-      .catch((err) => {
-        alert(err.message);
-      });
+    setIsLoading(false);
   };
 
   return (
     <Card raised>
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          {isLogin ? "Sign in to continue" : "Sign up to continue"}
+          {isSignin ? "Sign in to continue" : "Sign up to continue"}
         </Typography>
         <form onSubmit={submitHandler}>
           <TextField
@@ -154,9 +84,9 @@ export default function AuthForm({ isSignin }) {
             variant="outlined"
             inputRef={passwordInputRef}
           />
-          {!isLogin && (
+          {!isSignin && (
             <TextField
-              error={submitButtonClicked && !enteredConfirmPasswordIsValid}
+              error={submitButtonClicked && !enteredConfirmPasswordIsMatch}
               fullWidth
               label="Confirm Password"
               required
@@ -165,15 +95,18 @@ export default function AuthForm({ isSignin }) {
               inputRef={confirmPasswordInputRef}
             />
           )}
+          {isSignin && (
+            <CardActions>
+              <Button>Forgot password?</Button>
+            </CardActions>
+          )}
           <CardActions>
-            <Button component={NavLink} to={isLogin ? "/signup" : "/signin"}>
-              {isLogin ? "Create new account" : "Login with existing account"}
+            <Button component={NavLink} to={isSignin ? "/signup" : "/signin"}>
+              {isSignin ? "Create new account" : "Login with existing account"}
             </Button>
-          </CardActions>
-          <CardActions>
             {!isLoading && (
               <Button type="submit">
-                {isLogin ? "Login" : "Create Account"}
+                {isSignin ? "Login" : "Create Account"}
               </Button>
             )}
             {isLoading && <p>Sending request...</p>}
