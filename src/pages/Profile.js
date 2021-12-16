a2d6595a124e6f482a622c39a46c34427cdecdc0
DesmondTo <e0550619@u.nus.edu>
2021-12-16 18:37:40 +0800

    Implement profile photo changing feature

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,20 +1,86 @@
-import { useContext } from "react";
+import React, { useState, useContext } from "react";
 
-import Avatar from "@mui/material/Avatar";
+import styled from "styled-components";
+import {
+  deleteObject,
+  getDownloadURL,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import PhotoCamera from "@mui/icons-material/PhotoCamera";
 
 import AuthContext from "../store/auth-context";
+import { storage } from "../firebase/firebase";
+import CustomSnackbar from "../components/Messages/CustomSnackbar";
 
 function Profile() {
   const authCtx = useContext(AuthContext);
   const isSignedIn = authCtx.isSignedIn;
-  const profilePhotoURL = isSignedIn
-    ? authCtx.user.photoURL
-    : "./user_profile.png";
+  const [profilePhotoURL, setProfilePhotoURL] = useState(
+    isSignedIn ? authCtx.user.photoURL : "./user_profile.png"
+  );
+  const [showSnackbar, setShowSnackbar] = useState(false);
 
-  return isSignedIn ? (
-    <Avatar alt="User Profile Photo" src={profilePhotoURL} />
-  ) : null;
+  const uploadPhotoHandler = (event) => {
+    event.preventDefault();
+    const file = event.target.files[0];
+    uploadFile(file);
+  };
+
+  const uploadFile = (file) => {
+    if (!file) {
+      return;
+    }
+    const storageRef = ref(storage, `profiles/${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    const currentPhotoStorageRef = ref(storage, profilePhotoURL);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {},
+      (error) => alert(error.message),
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+          authCtx.updateProfilePhoto(url);
+          deleteObject(currentPhotoStorageRef);
+          setShowSnackbar(true);
+          setProfilePhotoURL(url);
+        });
+      }
+    );
+  };
+
+  return (
+    <>
+      {isSignedIn && <Avatar alt="User Profile Photo" src={profilePhotoURL} />}
+      <form>
+        <label htmlFor="contained-button-profile-photo">
+          <InvisibleInput
+            accept="image/*"
+            id="contained-button-profile-photo"
+            type="file"
+            onChange={uploadPhotoHandler}
+          />
+          <Button
+            component="span"
+            variant="contained"
+            endIcon={<PhotoCamera />}
+          >
+            Change profile picture
+          </Button>
+        </label>
+        {showSnackbar && (
+          <CustomSnackbar message="Profile photo updated" severity="success" />
+        )}
+      </form>
+    </>
+  );
 }
 
 export default Profile;
+
+const InvisibleInput = styled.input`
+  display: none;
+`;
