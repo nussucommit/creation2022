5e9b24c6285e73062d6782e048eac0d62666fd95
DesmondTo <e0550619@u.nus.edu>
2021-12-18 21:11:52 +0800

    Add text filed helper text constants

---
+++ src/constants/input/helper_text.js
@@ -0,0 +1,5 @@
+export const INPUT_HELPERTEXT_USERNAME = "Tip: At least 5 to 20 characters without whitespace. Allowed symbols: A-Z, a-z, 0-9, _.";
+
+export const INPUT_HELPERTEXT_EMAIL = "Example: e1234567@u.nus.edu or nus.friendly.mail_99@u.nus.edu";
+
+export const INPUT_HELPERTEXT_PASSWORD = "Tip: At least eight characters without whitespace. Allowed symbols: A-Z, a-z, 0-9, @$!%*#?&";
