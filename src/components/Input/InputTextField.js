3cb0999ee8bbac2f6e8ddcdc18a197bc9119f9da
DesmondTo <e0550619@u.nus.edu>
2021-12-19 00:05:12 +0800

    Add focus helper text

--- src/components/Input/InputTextField.js
+++ src/components/Input/InputTextField.js
@@ -1,13 +1,32 @@
+import { useMemo } from "react";
+
 import TextField from "@mui/material/TextField";
+import { useFormControl } from "@mui/material/FormControl";
+import FormHelperText from "@mui/material/FormHelperText";
+
+function TextFieldHelperText({ focusText }) {
+  const { focused } = useFormControl() || {};
+
+  const helperText = useMemo(() => {
+    if (focused) {
+      return focusText;
+    }
+
+    return "";
+  }, [focused, focusText]);
+
+  return <FormHelperText>{helperText}</FormHelperText>;
+}
 
 function InputTextField({ error, helperText, inputRef, label, type }) {
   return (
     <TextField
       error={error}
       fullWidth
-      helperText={helperText}
+      helperText={<TextFieldHelperText focusText={helperText} />}
       inputRef={inputRef}
       label={label}
+      margin="normal"
       required
       type={type}
       variant="standard"
