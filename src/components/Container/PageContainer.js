2a7a69fd84254fb913ecedc764a597759138779e
DesmondTo <e0550619@u.nus.edu>
2021-12-22 11:41:38 +0800

    Add universal page container

---
+++ src/components/Container/PageContainer.js
@@ -0,0 +1,26 @@
+import Grid from "@mui/material/Grid";
+
+function PageContainer({ childComponents }) {
+  return (
+    <Grid container direction="row" justifyContent="center" alignItems="center">
+      <Grid item xs />
+      <Grid item xs={10}>
+        {childComponents.map((component, index) => {
+          const isFirtstItem = index === 0;
+          return (
+            <Grid
+              item
+              textAlign={isFirtstItem && "center"}
+              sx={isFirtstItem && { m: "2rem" }}
+            >
+              {component}
+            </Grid>
+          );
+        })}
+      </Grid>
+      <Grid item xs />
+    </Grid>
+  );
+}
+
+export default PageContainer;
