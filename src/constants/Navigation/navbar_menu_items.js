b61fbf55031847b5eb226812acb5fa7432044254
DesmondTo <e0550619@u.nus.edu>
2021-12-24 03:48:14 +0800

    Rename file

---
+++ src/constants/Navigation/navbar_menu_items.js
@@ -0,0 +1,28 @@
+const menuItems = [
+  {
+    pageTitle: "Announcement",
+    pageURL: "/announcement",
+  },
+  {
+    pageTitle: "Challenges",
+    pageURL: "/challenges",
+  },
+  {
+    pageTitle: "Rules",
+    pageURL: "/rules",
+  },
+  {
+    pageTitle: "Submission",
+    pageURL: "/submission",
+  },
+  {
+    pageTitle: "FAQ",
+    pageURL: "/faq",
+  },
+  {
+    pageTitle: "Contact",
+    pageURL: "/contact",
+  },
+];
+
+export default menuItems;
