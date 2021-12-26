ffa2511747bb7617f2093cf1bd85c2c2abdb54c3
DesmondTo <e0550619@u.nus.edu>
2021-12-26 19:14:43 +0800

    Add support for announcement image upload

--- src/components/Announcement/AddAnnouncement.jsx
+++ src/components/Announcement/AddAnnouncement.jsx
@@ -1,20 +1,23 @@
-import { useRef, useContext } from "react";
+import { useState, useRef, useContext } from "react";
 
 import { collection, addDoc } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardHeader from "@mui/material/CardHeader";
 import TextField from "@mui/material/TextField";
+import Input from "@mui/material/Input";
 
-import { db } from "../../firebase/firebase-config";
+import { db, storage } from "../../firebase/firebase-config";
 import { getTimestamp, getDateTime } from "../../helpers/date-time-getter";
 import SnackbarContext from "../../store/snackbar-context";
 
 function AddAnnouncement({ isAdmin }) {
   const snackbarCtx = useContext(SnackbarContext);
-  
+  const [imageFile, setImageFile] = useState(null);
+
   const titleInputRef = useRef();
   const detailInputRef = useRef();
 
@@ -27,25 +30,57 @@ function AddAnnouncement({ isAdmin }) {
       type,
     });
 
+  const uploadHandler = (event) => {
+    event.preventDefault();
+
+    setImageFile(event.target.files[0]);
+  };
+
   const submitHandler = async (event) => {
     event.preventDefault();
 
     const enteredTitle = titleInputRef.current.value;
     const enteredDetail = detailInputRef.current.value;
     const currentTimestamp = getTimestamp();
+    const postDateTime = getDateTime(currentTimestamp);
 
-    await addDoc(announcementCollectionRef, {
-      title: enteredTitle,
-      timestamp: currentTimestamp,
-      dateTime: getDateTime(currentTimestamp),
-      detail: enteredDetail,
-    })
-      .then(() => setSnackbar("Announcement posted", "success"))
-      .then(() => {
-        titleInputRef.current.value = "";
-        detailInputRef.current.value = "";
+    const imageChosen = !!imageFile;
+
+    if (imageChosen) {
+      const modifiedImageName = `announcement_${postDateTime}`;
+      const imageStorageLocation = `announcements/${modifiedImageName}`;
+      const imageStorageRef = ref(storage, imageStorageLocation);
+      await uploadBytes(imageStorageRef, imageFile);
+      getDownloadURL(imageStorageRef)
+        .then(async (imageURL) => {
+          await addDoc(announcementCollectionRef, {
+            title: enteredTitle,
+            timestamp: currentTimestamp,
+            dateTime: getDateTime(currentTimestamp),
+            detail: enteredDetail,
+            imageURL,
+          });
+        })
+        .then(() => setSnackbar("Announcement posted", "success"))
+        .then(() => {
+          titleInputRef.current.value = "";
+          detailInputRef.current.value = "";
+        })
+        .catch((error) => setSnackbar(error.message, "error"));
+    } else {
+      await addDoc(announcementCollectionRef, {
+        title: enteredTitle,
+        timestamp: currentTimestamp,
+        dateTime: getDateTime(currentTimestamp),
+        detail: enteredDetail,
       })
-      .catch((error) => setSnackbar(error.message, "error"));
+        .then(() => setSnackbar("Announcement posted", "success"))
+        .then(() => {
+          titleInputRef.current.value = "";
+          detailInputRef.current.value = "";
+        })
+        .catch((error) => setSnackbar(error.message, "error"));
+    }
   };
 
   return isAdmin ? (
@@ -67,6 +102,13 @@ function AddAnnouncement({ isAdmin }) {
             fullWidth
             multiline
             inputRef={detailInputRef}
+          />
+          <Input
+            inputProps={{ accept: "image/*", multiple: true }}
+            type="file"
+            onChange={uploadHandler}
+            disableUnderline
+            sx={{ my: "1rem" }}
           />
         </CardContent>
         <CardActions>
