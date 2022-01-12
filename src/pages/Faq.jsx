5ceb344962da5d4deb6485a0b534c0b28dfca2a5
erinmayg <erinmaygunawan@gmail.com>
2022-01-12 20:54:29 +0800

    Update faq

--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -1,27 +1,32 @@
-import faqs from "../data/faq.json";
+import faqs from '../data/faq.json';
 
 function Faq() {
   return (
-    <div className="mainBody">
-      <h1
-        className="mainTitle"
-        style={{ textShadow: '0px 0px 16px #ED8F03' }}
-      >
+    <div className='mainBody'>
+      <h1 className='mainTitle' style={{ textShadow: '0px 0px 16px #ED8F03' }}>
         Frequently Asked Questions
       </h1>
       <br />
-      <div className="mainContent" style={{padding: "0 15vw"}}>
+      <ol className='mainContent' style={{ padding: '0 15vw' }}>
         {faqs.map((faq, index) => (
           <div key={index} style={{ marginBottom: '80px' }}>
-            <h2 className="secondaryTitle" style={{ letterSpacing: '1px' }}>
-              {index + 1}. {faq.question}
-            </h2>
-            <span style={{ color: 'white', lineHeight: '30px', whiteSpace:"pre-line" }}>
-              {faq.answer}
-            </span>
+            <li className='secondaryTitle' style={{ fontWeight: '800' }}>
+              <h2 className='secondaryTitle'>{faq.question}</h2>
+              <p
+                style={{
+                  color: 'white',
+                  lineHeight: '30px',
+                  whiteSpace: 'pre-line',
+                  fontSize: '1rem',
+                  fontWeight: 'normal',
+                }}
+              >
+                {faq.answer}
+              </p>
+            </li>
           </div>
         ))}
-      </div>
+      </ol>
     </div>
   );
 }
